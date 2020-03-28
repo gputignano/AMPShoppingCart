16411e2cde9298165e1a5a9e41d234aa5f0071d2
Giovanni Putignano <gputignano@gmail.com>
2020-03-28 09:45:25 +0100

    Updated composer.lock and TableSchemaTest class name

--- tests/Unit/TableSchemaTest.php
+++ tests/Unit/TableSchemaTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
-class DatabaseSchemaTest extends TestCase
+class TableSchemaTest extends TestCase
 {
     use RefreshDatabase;
 
