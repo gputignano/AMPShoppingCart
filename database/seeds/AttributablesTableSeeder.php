ce7d799341b77be63e98dc984b1d54950ced61f5
Giovanni Putignano <gputignano@gmail.com>
2020-04-07 22:43:48 +0200

    Seeded attributables table

--- database/seeds/AttributablesTableSeeder.php
+++ database/seeds/AttributablesTableSeeder.php
@@ -12,12 +12,41 @@ class AttributablesTableSeeder extends Seeder
      */
     public function run()
     {
-        Attribute::find(8)->attributables()->create([
-            'value' => false,
+        Attribute::find(4)->attributables()->createMany([ // Manufacturer
+            ['value' => 'Elena Fucci'],
+            ['value' => 'Feudi di San Gregorio'],
+            ['value' => 'Vietti'],
+            ['value' => 'Allegrini'],
+            ['value' => 'Cusumano'],
         ]);
 
-        Attribute::find(8)->attributables()->create([
-            'value' => true,
+        Attribute::find(5)->attributables()->createMany([ // Year
+            ['value' => '2015'],
+            ['value' => '2016'],
+            ['value' => '2017'],
+            ['value' => '2018'],
+            ['value' => '2019'],
+            ['value' => '2020'],
+        ]);
+
+        Attribute::find(6)->attributables()->createMany([ // Cultivar
+            ['value' => 'Coratine'],
+            ['value' => 'Moraiolo'],
+            ['value' => 'Bosana'],
+            ['value' => 'Biancolelle'],
+        ]);
+
+        Attribute::find(7)->attributables()->createMany([
+            ['value' => '5 minutes'],
+            ['value' => '10 minutes'],
+            ['value' => '15 minutes'],
+            ['value' => '20 minutes'],
+            ['value' => '25 minutes'],
+        ]);
+
+        Attribute::find(8)->attributables()->createMany([ // Active
+            ['value' => false],
+            ['value' => true],
         ]);
     }
 }
