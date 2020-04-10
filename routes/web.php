8361d95c94e12993493aee8cb25ba7a5e0ef9897
Giovanni Putignano <gputignano@gmail.com>
2020-04-10 17:13:42 +0200

    Preparing for Admin routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function() {
+    Route::get('/', function() {
+        return 'Admin Area';
+    });
 });
