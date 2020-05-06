bb273a6aa5251366181839cb5f69acf8c1706853
Giovanni Putignano <gputignano@gmail.com>
2020-05-06 21:54:33 +0200

    Added some link in admin home page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,5 +5,16 @@
 @section('content')
     <h1>{{ __('Admin Home') }}</h1>
 
+    <div>
+        <a href="{{ route('admin.orders.index') }}">{{ __('Orders') }}</a>
+    </div>
+
+    <div>
+        <a href="{{ route('admin.users.index') }}">{{ __('Users') }}</a>
+    </div>
+
+    <div>
+        <a href="{{ route('admin.products.index') }}">{{ __('Products') }}</a>
+    </div>
 @endsection
 
