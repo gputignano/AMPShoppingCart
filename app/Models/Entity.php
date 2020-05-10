b4e16ed66d3db94cd5171995ddcf787e97e9a394
Giovanni Putignano <gputignano@gmail.com>
2020-05-10 20:25:58 +0200

    Moved logic from AbstractEntity contract to Entity model

--- app/Models/Entity.php
+++ app/Models/Entity.php
@@ -2,9 +2,84 @@
 
 namespace App\Models;
 
-use App\Contracts\AbstractEntity;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 
-class Entity extends AbstractEntity
+class Entity extends Model
 {
-    //
+    public $table = 'entities';
+
+    protected $fillable = [
+        'parent_id', 'name', 'type',
+    ];
+
+    public $timestamps = false;
+
+    public function attributes()
+    {
+        return $this->belongsToMany(
+            Attribute::class,
+            'eavs',
+            'entity_id',
+            'attribute_id',
+            'id',
+            'id',
+            'attributes,'
+        );
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(
+            $this,
+            'parent_id',
+            'id',
+            'parent',
+        );
+    }
+
+    public function children()
+    {
+        return $this->hasMany(
+            $this,
+            'parent_id',
+            'id',
+        );
+    }
+
+    public function eavs($attribute_id = null)
+    {
+        return $this->hasMany(
+            EAV::class,
+            'entity_id',
+            'id',
+        )
+        ->when($attribute_id, function ($query, $attribute_id) {
+            return $query->where('attribute_id', $attribute_id );
+        });
+    }
+
+    public function rewrite()
+    {
+        return $this->hasOne(
+            Rewrite::class,
+            'entity_id',
+            'id',
+        );
+    }
+
+    protected static function booted()
+    {
+        parent::booted();
+
+        static::creating(function ($entity) {
+            $entity->forceFill([
+                'type' => static::class,
+            ]);
+        });
+
+        static::addGlobalScope('type', function (Builder $builder) {
+            $builder->where('type', static::class);
+        });
+    }
 }
