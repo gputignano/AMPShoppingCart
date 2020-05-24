3b410a49a60f1f775b6171fa1d2b71b65c692231
Giovanni Putignano <gputignano@gmail.com>
2020-05-24 09:20:16 +0200

    Added Front controller

---
+++ resources/views/front/home.blade.php
@@ -0,0 +1,5 @@
+@extends('front.layouts.main')
+
+@section('content')
+    <h1>{{ __('Home Page') }}</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'HomeController');
+
 Route::prefix('admin')->name('admin.')->group(function() {
     Route::view('/', 'admin.home')->name('home');
 
