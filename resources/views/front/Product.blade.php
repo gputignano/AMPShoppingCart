169bc417c4e5c4c829a243547af14082abd73e70
Giovanni Putignano <gputignano@gmail.com>
2020-05-25 16:38:40 +0200

    Improved Product blade template

--- resources/views/front/Product.blade.php
+++ resources/views/front/Product.blade.php
@@ -1,7 +1,11 @@
 @extends('front.layouts.main')
 
 @section('content')
-    <h1>{{ __('Product') }}</h1>
+    <h1>{{ $entity->name }}</h1>
 
-    {{ $entity }}
+    <div>{{ __('Price: ') . $entity->price }}</div>
+
+    <div>{{ __('Quantity: ') . $entity->quantity }}</div>
+
+    <div>{{ $entity->description }}</div>
 @endsection
