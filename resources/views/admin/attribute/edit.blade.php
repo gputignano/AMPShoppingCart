2164afac340582df9bf8253b9bdd29cbae173285
Giovanni Putignano <gputignano@gmail.com>
2020-06-06 15:54:37 +0200

    Improved style for admin attribute views

--- resources/views/admin/attribute/edit.blade.php
+++ resources/views/admin/attribute/edit.blade.php
@@ -16,80 +16,94 @@
     {{-- ATTRIBUTE VALUES --}}
     <amp-accordion id="attributes" expand-single-section animate>
         <section>
-            <h2>{{ __('General') }}</h2>
+            <h2 class="p1">{{ __('General') }}</h2>
 
-            <form method="post" action-xhr="{{ route('admin.attributes.update', $attribute) }}">
-                @csrf
-                @method('patch')
-
-                <fieldset>
-                    <label for="label">{{ __('Label') }}</label>
-                    <input type="text" name="label" value="{{ $attribute->label }}">
-                </fieldset>
+            <div class="mt2">
+                <form method="post" action-xhr="{{ route('admin.attributes.update', $attribute) }}">
+                    @csrf
+                    @method('patch')
     
-                <fieldset disabled>
-                    <label for="code">{{ __('Code') }}</label>
-                    <input type="text" name="code" value="{{ $attribute->code }}">
-                </fieldset>
+                    <div>
+                        <label for="label" class="label">{{ __('Label') }}</label>
+                        <input type="text" id="label" name="label" value="{{ $attribute->label }}" class="input border-green">
+                    </div>
+        
+                    <div>
+                        <label for="code" class="label">{{ __('Code') }}</label>
+                        <input type="text" id="code" name="code" value="{{ $attribute->code }}" class="input border-red" disabled>
+                    </div>
+        
+                    <div>
+                        <label for="type" class="label">{{ __('Type') }}</label>
+                        <input type="text" id="type" name="type" value="{{ class_basename($attribute->type) }}" class="input border-red" disabled>
+                    </div>
     
-                <fieldset disabled>
-                    <label for="type">{{ __('Type') }}</label>
-                    <input type="text" name="type" value="{{ class_basename($attribute->type) }}">
-                </fieldset>
-
-                <fieldset>
-                    <label for="is_system">{{ __('Is System') }}</label>
-                    <input type="checkbox" name="is_system" {{ $attribute->checked($attribute, 'is_system') }}>
-                </fieldset>
+                    <div>
+                        <label for="is_system" class="label">
+                            <input type="checkbox" id="is_system" name="is_system" {{ $attribute->checked($attribute, 'is_system') }} class="checkbox">
+                            {{ __('Is System') }}
+                        </label>
+                    </div>
+        
+                    <div>
+                        <label for="is_visible_on_front" class="label">
+                            <input type="checkbox" id="is_visible_on_front" name="is_visible_on_front" class="checkbox" {{ $attribute->checked($attribute, 'is_visible_on_front') }}>
+                            {{ __('Is Visible on Front') }}
+                        </label>
+                    </div>
     
-                <fieldset>
-                    <label for="is_visible_on_front">{{ __('Is Visible on Front') }}</label>
-                    <input type="checkbox" name="is_visible_on_front" {{ $attribute->checked($attribute, 'is_visible_on_front') }}>
-                </fieldset>
-
-                <input type="submit" value="{{ __('Update') }}">
-
-                @include('admin.inc.response')
-            </form>
+                    <input type="submit" value="{{ __('Update') }}" class="btn btn-primary col-12 btn-big">
+    
+                    @include('admin.inc.response')
+                </form>
+            </div>
         </section>
 
         @if ($attribute->type::$hasDefaultValues)
         <section>
-            <h2>{{ __('Attribute Values') }}</h2>
+            <h2 class="p1">{{ __('Attribute Values') }}</h2>
 
-            <form method="post" action-xhr="{{ route('admin.attributes.update', $attribute) }}">
-                @csrf
-                @method('patch')
-
-                <ul>
-                    @forelse ($attribute->values as $value)
-                        <li>
-                            {{ $value->value }}
-                        </li>
-                    @empty
-                        <li>{{ __('No attribute found') }}</li>
-                    @endforelse
-                </ul>
+            <div>
+                <form method="post" action-xhr="{{ route('admin.attributes.update', $attribute) }}">
+                    @csrf
+                    @method('patch')
     
-                @if ($attribute->type::$hasDefaultValues)
-                    <fieldset>
-                        <label for="value">{{ __('Value') }}</label>
-                        <input type="text" name="value">
-                    </fieldset>
-                @endif
-
-                <input type="submit" value="{{ __('Update') }}">
-
-                @include('admin.inc.response')
-            </form>
+                    <ul>
+                        @forelse ($attribute->values as $value)
+                            <li>
+                                {{ $value->value }}
+                            </li>
+                        @empty
+                            <li>{{ __('No attribute found') }}</li>
+                        @endforelse
+                    </ul>
+        
+                    @if ($attribute->type::$hasDefaultValues)
+                        <div>
+                            <label for="value" class="label">{{ __('Value') }}</label>
+                            <input type="text" id="value" name="value" class="input">
+                        </div>
+                    @endif
+    
+                    <input type="submit" value="{{ __('Update') }}" class="btn btn-primary col-12 btn-big">
+    
+                    @include('admin.inc.response')
+                </form>
+            </div>
         </section>
         @endif
+
+        <section class="mt2">
+            <h2 class="p1">{{ __('Delete Attribute') }}</h2>
+
+            <div class="mt2">
+                <form action-xhr="{{ route('admin.attributes.destroy', $attribute) }}" method="post">
+                    @csrf
+                    @method('delete')
+            
+                    <input type="submit" value="{{ __('Delete') }}" class="btn btn-primary col-12 bg-red btn-big" {{ $attribute->products->count() ? 'disabled' : '' }}>
+                </form>
+            </div>
+        </section>
     </amp-accordion>
-
-    <form action-xhr="{{ route('admin.attributes.destroy', $attribute) }}" method="post">
-        @csrf
-        @method('delete')
-
-        <input type="submit" value="{{ __('Delete') }}" {{ $attribute->products->count() ? 'disabled' : '' }}>
-    </form>
 @endsection
